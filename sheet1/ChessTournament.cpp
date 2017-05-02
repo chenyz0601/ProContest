9470e065f49b7e686edb049f90b81d071dc4b8ac
chenyz0601 <chenyz0601@gmail.com>
2017-05-02 20:02:55 +0200

    sheet1

--- sheet1/ChessTournament.cpp
+++ sheet1/ChessTournament.cpp
@@ -2,6 +2,19 @@
 
 using namespace std
 
+void print(int& a, int t, int& N){
+  for (int i = 0; i < t; i ++){
+    cout<<"Case #"<<i+1<<": "<<endl;
+    for (int j = 0; j < N[i]; j ++)
+      for (int k = 0; k < 5; k ++){
+        if (k == 4)
+          cout<<a[j][k]<<endl;
+        else
+          cout<<a[j][k]<<" ";
+      }
+  }
+}
+
 int main(){
   int t, n;
   string t_str, n_str;
@@ -11,11 +24,13 @@ int main(){
   t = stoi(t_str);
   if (t<1 || t>20)
     cout<<"invalid t"<<endl;
+  int N[t];
   for (int i = 0; i < t; i++){
     getline(cin, n_str);
     n = stoi(n_str);
     if (n<2 || n>1000)
       cout<<"invalid n"<<endl;
+    N[i] = n;
     int s[n][5];
     for (int j = 0; j < n; j ++){
       getline(cin, temp);
