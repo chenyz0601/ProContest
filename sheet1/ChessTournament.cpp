ca5e0d3101d0cff1c8f6bc8ec5e4b9ab090c8f0c
Yuanze Chen <chenyua@in.tum.de>
2017-05-02 21:14:03 +0200

    finish all

--- sheet1/ChessTournament.cpp
+++ sheet1/ChessTournament.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
+#include <iostream> 
+#include <algorithm>
+#include <functional>
 
-using namespace std
+using namespace std;
 
-void print(int& a, int t, int& N){
-  for (int i = 0; i < t; i ++){
-    cout<<"Case #"<<i+1<<": "<<endl;
-    for (int j = 0; j < N[i]; j ++)
-      for (int k = 0; k < 5; k ++){
-        if (k == 4)
-          cout<<a[j][k]<<endl;
-        else
-          cout<<a[j][k]<<" ";
-      }
+void print(int a[][5], int t, int N){
+  cout<<"Case #"<< t+1<<": "<<endl;
+  for (int j = 0; j < N; j ++){
+    for (int k = 0; k < 5; k ++){
+      if (k == 4)
+        cout<<a[j][k]<<endl;
+      else
+        cout<<a[j][k]<<" ";
+    }
   }
 }
+
 
 int main(){
   int t, n;
   string t_str, n_str;
   string temp;
-  size_type sz;
+  string blanks;
+  string::size_type sz;
   getline(cin, t_str);
   t = stoi(t_str);
   if (t<1 || t>20)
     cout<<"invalid t"<<endl;
+  int s[t][1000][5];
   int N[t];
   for (int i = 0; i < t; i++){
     getline(cin, n_str);
@@ -31,13 +35,29 @@ int main(){
     if (n<2 || n>1000)
       cout<<"invalid n"<<endl;
     N[i] = n;
-    int s[n][5];
     for (int j = 0; j < n; j ++){
       getline(cin, temp);
       for (int k = 0; k < 5; k ++){
-        s[j][k] = stoi(temp, &sz);
-        temp = temp.substr(sz+1);
+        s[i][j][k] = stoi(temp, &sz);
+        if (k<4)
+          temp = temp.substr(sz+1);
       }
+      sort(s[i][j], s[i][j]+5, greater<int>());
     }
+    int temp_s[n];
+    for (int k = 0; k < 5; k ++){
+      for (int j = 0; j < n; j ++)
+        temp_s[j] = s[i][j][k];
+      sort(temp_s, temp_s+n, greater<int>());
+      for (int j = 0; j < n; j ++)
+        s[i][j][k] = temp_s[j];
+    }
+    if (i < t-1)
+      getline(cin, blanks);
   }
+
+  for (int i = 0; i < t; i ++)
+    print (s[i], i, N[i]);
+
+  return 0;
 }
