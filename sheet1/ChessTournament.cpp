a97617629176a1e8ca5a5bba470ac087064d00c1
Yuanze Chen <chenyua@in.tum.de>
2017-05-02 21:31:44 +0200

    adding comments

--- sheet1/ChessTournament.cpp
+++ sheet1/ChessTournament.cpp
@@ -27,24 +27,33 @@ int main(){
   t = stoi(t_str);
   if (t<1 || t>20)
     cout<<"invalid t"<<endl;
-  int s[t][1000][5];
-  int N[t];
+  int s[t][1000][5]; // store arrays for all cases
+  int N[t]; // store n for each case
+
+  // loop over cases
   for (int i = 0; i < t; i++){
     getline(cin, n_str);
-    n = stoi(n_str);
+    n = stoi(n_str); // get n
     if (n<2 || n>1000)
       cout<<"invalid n"<<endl;
     N[i] = n;
+
+    // loop over current case
     for (int j = 0; j < n; j ++){
       getline(cin, temp);
+
+      // get a row and sort it
       for (int k = 0; k < 5; k ++){
         s[i][j][k] = stoi(temp, &sz);
         if (k<4)
-          temp = temp.substr(sz+1);
+          temp = temp.substr(sz+1); // skip a space
       }
       sort(s[i][j], s[i][j]+5, greater<int>());
     }
-    int temp_s[n];
+
+    int temp_s[n]; // temp array for columns
+
+    // sort each column
     for (int k = 0; k < 5; k ++){
       for (int j = 0; j < n; j ++)
         temp_s[j] = s[i][j][k];
@@ -52,10 +61,13 @@ int main(){
       for (int j = 0; j < n; j ++)
         s[i][j][k] = temp_s[j];
     }
+
+    // skip blank lines
     if (i < t-1)
       getline(cin, blanks);
   }
 
+  // print result
   for (int i = 0; i < t; i ++)
     print (s[i], i, N[i]);
 
